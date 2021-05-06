929de257755539be0285cf18b1606b8d5cb7ca8a
Alexander <alexander.bugara@gmail.com>
2021-05-06 16:25:51 +0000

    First user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email)
+    
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    #gravatar_url = "https://secure.gravatar.com/avatar/1fdaa466cbec3badf5418269ffc5968"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end 
+
 end
