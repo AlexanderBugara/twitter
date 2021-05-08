fafef1ca4be9330b38edf2a67736c4c68b4ed537
Alexander <alexander.bugara@gmail.com>
2021-05-08 14:24:44 +0000

    Drop down menu login session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'home'   => 'static_pages#home'
-  get 'help'   => 'static_pages#help'
-  get 'about'  => 'static_pages#about'
-  get 'signup' => 'users#new'
-  root 'static_pages#home'
+  get    'sessions/new'
+  get    'users/new'
+  get    'home'   => 'static_pages#home'
+  get    'help'   => 'static_pages#help'
+  get    'about'  => 'static_pages#about'
+  get    'signup' => 'users#new'
+  get    'login' => 'sessions#new'
+  post   'login' => 'sessions#create'
+  get    'logout' => 'sessions#destroy'
+  root   'static_pages#home'
   resources :users
 end
